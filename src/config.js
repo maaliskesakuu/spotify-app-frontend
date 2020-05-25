9c4e330143a8e764607cd849507749227f50b3a5
maaliskesakuu <tanja@prodi.fi>
2020-05-25 21:48:35 +0300

    tested Joe Karlsson's code

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+export const clientId = 'cc0b23ed0ef842bdba9556fd9aad1863';
+export const redirectUri = 'http://localhost:3000/callback';
+export const scopes = [
+  'user-top-read',
+  'user-read-currently-playing',
+  'user-read-playback-state',
+];
