ad25cc31e1b4656d281e5104b9f5b05089caa4d3
oona-k <59806531+oona-k@users.noreply.github.com>
2020-06-03 12:17:25 +0300

    Removed TrackResults.js and combined it with Activities.js. Added SearchResults, TrackList and Track from Tanja's branch.

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import TrackResults from "./TrackResults";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+//for getting token:
+import hash from "../hash";
+
+import SearchResults from "./SearchResults";
 
 const activities = [
   {
     id: 1,
     activity: "Study",
-    category_id: "focus", //Spotify doesn't have "study" category
+    category_id: "focus",
   },
   {
     id: 2,
@@ -22,30 +28,115 @@ const activities = [
   {
     id: 4,
     activity: "Meditate",
-    category_id: "wellness", //Spotify doesn't have "meditate" category
+    category_id: "wellness",
   },
 ];
 
 class Activities extends Component {
-  state = {
-    activities: activities,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activities: activities,
+      selectedCategory: "",
+      track: [],
+    };
+
+    this.activityButtonClicked = this.activityButtonClicked.bind(this);
+    this.getMusic = this.getMusic.bind(this);
+  }
+
+  getMusic() {
+    let token = hash.access_token;
+    fetch(
+      `https://api.spotify.com/v1/browse/categories/${this.state.selectedCategory}/playlists?limit=2`, //API call to get playlists by selected category
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.playlists) {
+          return [];
+        }
+
+        //Making array with playlist IDs
+        var ID_array = [];
+        jsonResponse.playlists.items.forEach((item) => {
+          ID_array.push(item.id);
+        });
+        console.log(ID_array);
+
+        return ID_array;
+      })
+      .then((ID_array) => {
+        for (var i = 0; i < ID_array.length; i++) {
+          fetch(
+            `https://api.spotify.com/v1/playlists/${ID_array[i]}/tracks?limit=10`, //API call with playlists IDs to get tracks
+            {
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+            .then((response) => {
+              return response.json();
+            })
+            .then((responseInJson) => {
+              console.log(responseInJson);
+              var mappedTracks = responseInJson.items.map((item) => {
+                return {
+                  id: item.track.id,
+                  track: item.track.name,
+                  artist: item.track.artists[0].name,
+                  album: item.track.album.name,
+                  preview: item.track.preview_url,
+                };
+              });
+              console.log(mappedTracks);
+
+              this.setState({ track: this.state.track.concat(mappedTracks) }); //concat to join results from each api call. Is it working correctly?
+              console.log(this.state.track);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error.data);
+      });
+  }
+
+  activityButtonClicked(button) {
+    this.setState({ track: [] }); //Making tracks empty before making new api call
+    this.setState({ selectedCategory: button.target.value }, () => {
+      this.getMusic();
+    });
+  }
 
   render() {
     const activityList = this.state.activities.map((activity) => {
       return (
-        <TrackResults
-          key={activity.id}
-          activity={activity.activity}
-          category_id={activity.category_id}
-        />
+        <Button
+          style={{ margin: "10px" }}
+          onClick={this.activityButtonClicked}
+          value={activity.category_id}
+        >
+          {activity.activity}
+        </Button>
       );
     });
 
     return (
       <Container>
         <h2>What you want to do?</h2>
-        {activityList}
+        <Row>{activityList}</Row>
+        <SearchResults searchResults={this.state.track} />
       </Container>
     );
   }
---
+++ src/components/Track.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+//import './Track.css';
+
+class Track extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(event) {
+    this.props.onAdd(this.props.track);
+  }
+
+  removeTrack(event) {
+    this.props.onRemove(this.props.track);
+  }
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action" onClick={this.removeTrack}>-</button>;
+    }
+    return <button className="Track-action" onClick={this.addTrack}>+</button>;
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+          <iframe
+            src={'https://open.spotify.com/embed/track/' + this.props.track.id}
+            width="300"
+            height="80"
+            frameBorder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+            title="preview"
+          />
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
+
+export default Track;
