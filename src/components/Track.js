7b3e4c4826f4ce9f244fa04aee6606cb88aadc2d
oona-k <59806531+oona-k@users.noreply.github.com>
2020-06-08 15:26:49 +0300

    Added conditional tooltip

--- src/components/Track.js
+++ src/components/Track.js
@@ -4,6 +4,8 @@ import React, { Component } from "react";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 class Track extends Component {
   constructor(props) {
@@ -16,6 +18,7 @@ class Track extends Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.playMusic = this.playMusic.bind(this);
     this.pauseMusic = this.pauseMusic.bind(this);
+    this.renderTooltip = this.renderTooltip.bind(this);
   }
 
   addTrack(event) {
@@ -25,16 +28,17 @@ class Track extends Component {
   removeTrack(event) {
     this.props.onRemove(this.props.track);
   }
-  playMusic() {
-    console.log("Play music");
-    if (this.props.track.preview) {
-      this.setState({ audio: new Audio(this.props.track.preview) }, () => {
-        this.state.audio.play();
-      });
-    } else {
-      console.log("no preview");
-    }
+
+  renderTooltip(props) {
+    return <Tooltip {...props}>Sorry, no preview</Tooltip>;
   }
+
+  playMusic() {
+    this.setState({ audio: new Audio(this.props.track.preview) }, () => {
+      this.state.audio.play();
+    });
+  }
+
   pauseMusic() {
     this.state.audio.pause();
     this.setState({ audio: new Audio("") });
@@ -43,17 +47,13 @@ class Track extends Component {
   renderAction() {
     if (this.props.isRemoval) {
       return (
-        <Button className="Track-action" onClick={this.removeTrack}>
+        <Button onClick={this.removeTrack} style={{ marginLeft: "10px" }}>
           -
         </Button>
       );
     }
     return (
-      <Button
-        className="Track-action"
-        onClick={this.addTrack}
-        style={{ marginLeft: "10px" }}
-      >
+      <Button onClick={this.addTrack} style={{ marginLeft: "10px" }}>
         +
       </Button>
     );
@@ -63,28 +63,29 @@ class Track extends Component {
     return (
       <Col md={3}>
         <Card style={{ margin: "10px" }}>
-          {/*  <h3>{this.props.track.name}</h3>
-          <p>
-            {this.props.track.artist} | {this.props.track.album}
-          </p> */}
-
           {/* Cards */}
-
-          <Card.Img
-            variant="top"
-            src={this.props.track.img}
-            onMouseOver={this.playMusic}
-            onMouseOut={this.pauseMusic}
-          />
+          {/* Conditional tooltips */}
+          {!this.props.track.preview ? (
+            <OverlayTrigger placement="bottom" overlay={this.renderTooltip}>
+              <Card.Img variant="top" src={this.props.track.img} />
+            </OverlayTrigger>
+          ) : (
+            <Card.Img
+              variant="top"
+              src={this.props.track.img}
+              onMouseOver={this.playMusic}
+              onMouseOut={this.pauseMusic}
+            />
+          )}
           <Card.Body>
             <Card.Text>
-              {this.props.track.artist} - {this.props.track.track}
+              {this.props.track.track} - {this.props.track.artist}
             </Card.Text>
-          </Card.Body> 
+          </Card.Body>
 
-           
           {/* <iframe
-            src={"https://open.spotify.com/embed/track/" + this.props.track.id}
+            src={'https://open.spotify.com/embed/track/' + this.props.track.id}
+            width="300"
             height="80"
             frameBorder="0"
             allowtransparency="true"
