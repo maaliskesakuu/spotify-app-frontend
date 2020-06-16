cfe85b9703bc7fac10d7261b7501ee041ea4e4dd
oona-k <59806531+oona-k@users.noreply.github.com>
2020-06-16 22:10:25 +0300

    Added onTouchStart and onTouchEnd to Track.js

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-// import './Track.css';
 
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
+
+import "../FontawesomeIcons/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Track extends Component {
   constructor(props) {
@@ -19,6 +21,8 @@ class Track extends Component {
     this.playMusic = this.playMusic.bind(this);
     this.pauseMusic = this.pauseMusic.bind(this);
     this.renderTooltip = this.renderTooltip.bind(this);
+    this.playMusicOnTouch = this.playMusicOnTouch.bind(this);
+    this.pauseMusicOnTouch = this.pauseMusicOnTouch.bind(this);
   }
 
   addTrack(event) {
@@ -33,32 +37,45 @@ class Track extends Component {
     return <Tooltip {...props}>Sorry, no preview</Tooltip>;
   }
 
-  playMusic() {
+   playMusic() {
+    if(window.innerWidth > 1279){
+    this.setState({ audio: new Audio(this.props.track.preview) }, () => {
+      this.state.audio.play();
+    });
+  } }
+
+  playMusicOnTouch() {
     this.setState({ audio: new Audio(this.props.track.preview) }, () => {
       this.state.audio.play();
     });
   }
 
-  pauseMusic() {
+   pauseMusic() {
+     if(window.innerWidth > 1279){
+    this.state.audio.pause();
+    this.setState({ audio: new Audio("") });
+     }
+  } 
+
+  pauseMusicOnTouch() {
     this.state.audio.pause();
     this.setState({ audio: new Audio("") });
   }
 
-     renderAction() {
+  renderAction() {
     if (this.props.isRemoval) {
       return (
-
         <Button
           onClick={this.removeTrack}
           style={{
-            position: 'absolute',
-            right: '10px',
-            bottom: '10px',
-            backgroundColor: 'cadetblue',
-            border: 'none',
+            position: "absolute",
+            right: "10px",
+            bottom: "10px",
+            backgroundColor: "#C40000",
+            border: "none",
           }}
         >
-          -
+          <FontAwesomeIcon icon="minus"></FontAwesomeIcon>
         </Button>
       );
     }
@@ -67,22 +84,22 @@ class Track extends Component {
       <Button
         onClick={this.addTrack}
         style={{
-          position: 'absolute',
-          right: '10px',
-          bottom: '10px',
-          backgroundColor: 'cadetblue',
-          border: 'none',
+          position: "absolute",
+          right: "10px",
+          bottom: "10px",
+          backgroundColor: "rgb(126, 2, 214)",
+          border: "none",
         }}
       >
-        +
+        <FontAwesomeIcon icon="plus"></FontAwesomeIcon>
       </Button>
-    ); 
-  } 
+    );
+  }
 
   render() {
     return (
       <Col md={3}>
-        <Card style={{ margin: "10px", boxShadow:"0 0 10px #333" }}>
+        <Card style={{ margin: "10px", boxShadow: "0 0 10px #333" }}>
           {/* Cards */}
           {/* Conditional tooltips */}
           {!this.props.track.preview ? (
@@ -93,12 +110,15 @@ class Track extends Component {
             <Card.Img
               variant="top"
               src={this.props.track.img}
+              onTouchStart={this.playMusicOnTouch}
+              onTouchEnd={this.pauseMusicOnTouch}
               onMouseOver={this.playMusic}
               onMouseOut={this.pauseMusic}
             />
           )}
-          <Card.Body style={{ minHeight: '7rem', padding: '10px' }}>
-
+          <Card.Body
+            style={{ height: "6.5rem", padding: "10px", overflow: "scroll" }}
+          >
             <Card.Text>
               {this.props.track.name} | {this.props.track.artist}
             </Card.Text>
